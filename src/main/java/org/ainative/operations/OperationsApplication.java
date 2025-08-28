22d71221420f68aac40c475db859444a8a752680
Alex <alex09936200@gmail.com>
2025-08-28 17:29:40 +0800

    test

--- src/main/java/org/ainative/operations/OperationsApplication.java
+++ src/main/java/org/ainative/operations/OperationsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OperationsApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(OperationsApplication.class, args);
     }
