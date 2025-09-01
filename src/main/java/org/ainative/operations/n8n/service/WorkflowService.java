e7645f15ad084e9cc065ae2c88b77e4e5b392a37
Alex <alex09936200@gmail.com>
2025-09-01 12:17:45 +0800

    flow

--- src/main/java/org/ainative/operations/n8n/service/WorkflowService.java
+++ src/main/java/org/ainative/operations/n8n/service/WorkflowService.java
@@ -12,22 +12,27 @@ import java.io.IOException;
 public class WorkflowService {
 
 
-    public ResponseBody getFlows(String tag,Boolean active) {
+    public ResponseBody getFlows(String tag, Boolean active) {
 
         OkHttpClient client = new OkHttpClient();
 
-        String url = "http://31.97.131.76:5678/api/v1/workflows?projectId=VmwOO9HeTEj20kxM&excludePinnedData=false&limit=30&tags=" + tag+"active="+active;
+        String url = "http://31.97.131.76:5678/api/v1/workflows";
+        if (tag != null) {
+            url = url + "?tags=" + tag;
+        }
+        if (active != null) {
+            url = url + "&active=" + active;
+        }
+
         Request request = new Request.Builder()
-                .header("Accept", "*/*")
                 .url(url)
                 .get()
-                .addHeader("X-N8N-API-KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI4Yjg1YzcwZC02ZWEwLTRiYzQtYWM4Yi01ZGYxNzM0YjQ3MzciLCJpc3MiOiJuOG4iLCJhdWQiOiJwdWJsaWMtYXBpIiwiaWF0IjoxNzU1NjYzNTcyfQ.3NsQ1t_4QIx8iFnQVTEx0U_t_RupRijbrdycbP2gGNg")
+                .addHeader("X-N8N-API-KEY", "eeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI4Yjg1YzcwZC02ZWEwLTRiYzQtYWM4Yi01ZGYxNzM0YjQ3MzciLCJpc3MiOiJuOG4iLCJhdWQiOiJwdWJsaWMtYXBpIiwiaWF0IjoxNzU1NjYzNTcyfQ.3NsQ1t_4QIx8iFnQVTEx0U_t_RupRijbrdycbP2gGNg")
                 .build();
 
-        try {
-            System.out.println(url);
-            Response response = client.newCall(request).execute();
+        System.out.println(url);
 
+        try(Response response = client.newCall(request).execute()) {
             return response.body();
         } catch (IOException e) {
             throw new RuntimeException(e);
