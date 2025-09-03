bcebd36b1b87f055c0ec33c74c5ba68eaadc6185
史作新 <zuoxin.shi@daocloud.io>
2025-09-03 11:45:13 +0800

    添加配置文件解析N8nConfig

---
+++ src/main/java/org/ainative/operations/config/N8nConfig.java
@@ -0,0 +1,14 @@
+package org.ainative.operations.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "n8n")
+public class N8nConfig {
+    String baseurl;
+    String apikey;
+    String projectId;
+}
